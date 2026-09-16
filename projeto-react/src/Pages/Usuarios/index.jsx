a14f57c7e783cc2a422f81c0503f9a74b45789f0
ThiagoRelias <thiagorelias04052007@gmail.com>
2026-09-16 11:01:17 -0300

    feat: updated

--- projeto-react/src/Pages/Usuarios/index.jsx
+++ projeto-react/src/Pages/Usuarios/index.jsx
@@ -1,26 +1,32 @@
 import { useState } from "react"
 
 export default function index() {
-  const [usuarios, setUsuarios] = useState([]);
+    const [usuarios, setUsuarios] = useState([]);
 
-  userEffect(() => {
-    fetch("http://localhost:3000/usuarios")
-      .then((response) => response.json())
-      .then((data) => setUsuarios(data))
-      .catch((error) => console.error(error));
-  }, [])
-  return (
-    <section className="container-usuarios">
-        <h1>Lista de Usuários</h1>
+    userEffect(() => {
+        fetch("http://localhost:3000/usuarios")
+            .then((response) => response.json())
+            .then((data) => setUsuarios(data))
+            .catch((error) => console.error(error));
+    }, [])
+    return (
+        <section className="container-usuarios">
+            <h1>Lista de Usuários</h1>
 
-        <article className="usuarios">
-            <strong>Nome: Caio</strong>
-               <br />
-            <strong>Telefone: (11) 99999-9999</strong>
-            <br />
-            <button className="delete">Deletar</button>
-            <br />
-        </article>
-    </section>
-  )
+            {usuarios.map((usuario) => (
+
+                <article className="content-usuarios" key={usuario.id}>
+                    <strong>Nome: {usuario.nome}</strong>
+                    <br />
+                    <strong>Telefone: {usuario.telefone}</strong>
+                    <br />
+                    <button className="delete">Deletar</button>
+                    <br />
+                </article>
+
+            ))}
+
+
+        </section>
+    )
 }
